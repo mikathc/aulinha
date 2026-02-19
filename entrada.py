14d6bc3ed38010a40c831ffa4b396cfb685ee57c
Your Name <you@example.com>
2026-02-19 10:56:23 -0300

    final aula de string

--- entrada.py
+++ entrada.py
@@ -15,3 +15,20 @@ print('Colocando o texto em maiusculo:', palavra.upper())
 PALAVRA = 'SITE'
 print('Colocando o texto em minusculo:', PALAVRA.lower())
 
+palavra_contar = 'banana'
+print('Contar a letra b', palavra_contar.count('b'))
+print('Contar a letra a', palavra_contar.count('a'))
+print('Contar a letra n', palavra_contar.count('n'))
+print('Contar a letra a', palavra_contar.count('a'))
+print('Contar a letra n', palavra_contar.count('n'))
+print('Contar a letra a', palavra_contar.count('a'))
+
+email2 = email
+letraA= email2.count ('a')
+letraE= email2.count ('e')
+letraI= email2.count ('i')
+letraO= email2.count ('o')
+letraU= email2.count ('o')
+
+senha = 'a'+str(letraA) + 'e'+str(letraE) + 'i'+str(letraI) + 'o'+str(letraO) + 'u'+str(letraU)
+print('sua senha é:', senha)
--- inputs_python.py
+++ inputs_python.py
@@ -1,3 +1,3 @@
-kath
-123
+kathfofinha
+122333444455555
 gmail.com
